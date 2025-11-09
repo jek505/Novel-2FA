54ffc7cb2a90858f6c054a74c0699154035289f2
jek505 <grxzaky@gmail.com>
2025-11-09 19:37:15 +0700

    fix passport export for vercel

--- config/passport.js
+++ config/passport.js
@@ -1,38 +1,42 @@
-const passport = require('passport');
+// config/passport.js
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const User = mongoose.model('User');
+module.exports = (passport) => {
+  const User = mongoose.model('User');
 
-// Local strategy for authentication
-passport.use(
-  new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+  // Local strategy for authentication
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
+      try {
+        const user = await User.findOne({ email: email.toLowerCase() });
+        if (!user) {
+          return done(null, false, { message: 'Email tidak terdaftar.' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.passwordHash);
+        if (!isMatch) {
+          return done(null, false, { message: 'Password salah.' });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    })
+  );
+
+  // Serialize user id into session
+  passport.serializeUser((user, done) => done(null, user.id));
+
+  // Deserialize user object from id in session
+  passport.deserializeUser(async (id, done) => {
     try {
-      const user = await User.findOne({ email: email.toLowerCase() });
-      if (!user) {
-        return done(null, false, { message: 'Email tidak terdaftar.' });
-      }
-      const isMatch = await bcrypt.compare(password, user.passwordHash);
-      if (!isMatch) {
-        return done(null, false, { message: 'Password salah.' });
-      }
-      return done(null, user);
+      const user = await User.findById(id);
+      done(null, user);
     } catch (err) {
-      return done(err);
+      done(err);
     }
-  })
-);
-
-// Serialise user id into session
-passport.serializeUser((user, done) => done(null, user.id));
-
-// Deserialize user object from id in session
-passport.deserializeUser(async (id, done) => {
-  try {
-    const user = await User.findById(id);
-    done(null, user);
-  } catch (err) {
-    done(err);
-  }
-});
+  });
+};
